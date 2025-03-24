322383c6f6db719108a79fcac224e57c078ce3fd
lurah <acarnila@gmail.com>
2025-03-24 15:43:08 +0700

    Finish

--- project.py
+++ project.py
@@ -81,8 +81,11 @@ class Warna():
                     min-width: 50px; padding: 5px; border: 1px solid {warna}"
 
         lbl = Label(f"{rgbhsl.capitalize()}", fr=f"rgb_{rgbhsl}", style=lbl_sty)
-        inp = Input(name=f"rgb_{rgbhsl}", id=f"rgb_{rgbhsl}", type="text",
-                    value=f"{nilai}", style=inp_sty)
+        val = {"tipe":tipe, "ist": self.warnanya(), "rgbhsl": rgbhsl}
+        prm = {"type":"text", "hx_swap":"innerHTML", "hx_post":"/flt_nl", "hx_trigger":"change",
+               "hx_target":"#tengah", "hx_vals": val}
+        inp = Input(name=f"rgb_{rgbhsl}", id=f"rgb_{rgbhsl}",
+                    value=f"{nilai}", style=inp_sty, **prm)
         return Div(inp, lbl, style=div_sty)
     
     def teks(self, tipe:str):
@@ -313,13 +316,40 @@ def name_kl():
         return kotak.kotak_warna(), kotak.teks("rgb"), kotak.teks("hsl"), \
                kotak.slider_rgb(), kotak.slider_hsl()
     return lk_name_kl
-    
+
+def flt_nl():
+    lk_filter = APIRouter()
+    @lk_filter.post("/flt_nl")
+    def filter(tipe:str, ist:str, rgbhsl:str, rgb_red:str=None, rgb_blue:str=None,
+               rgb_green:str=None, rgb_hue:str=None, rgb_saturation:str=None,
+               rgb_lightness:str=None):
+        rgb = [int(kl) for kl in ast.literal_eval(ist.strip("rgb"))]
+        if tipe == "rgb":
+            match rgbhsl:
+                case "red": rgb[0] = min(int(rgb_red),255)
+                case "green": rgb[1] = min(int(rgb_green),255)
+                case "blue": rgb[2] = min(int(rgb_blue),255)
+        else:
+            hsl = {key:int(value) for key,value in Warna.rgb_to_hsl(ist).items()}
+            match rgbhsl:
+                case "hue": hsl["hue"] = min(int(rgb_hue),360)
+                case "saturation": hsl["saturation"] = min(int(rgb_saturation),100)
+                case "lightness": hsl["lightness"] = min(int(rgb_lightness),100)
+            rgb = Warna.hsl_to_rgb(hsl["hue"], hsl["saturation"], hsl["lightness"])
+        warnanya = {"red":f"{rgb[0]}","green":f"{rgb[1]}","blue":f"{rgb[2]}"}
+        kotak = Warna(**warnanya)    
+        return kotak.kotak_warna(), kotak.teks("rgb"), kotak.teks("hsl"), \
+               kotak.slider_rgb(), kotak.slider_hsl(), \
+               kotak.klr_isi()
+    return lk_filter
+
 def main():
     app = FastHTML()
     semua().to_app(app)
     tengah().to_app(app)
     ganti_klr().to_app(app)
     name_kl().to_app(app)
+    flt_nl().to_app(app)
     return app
 
 
